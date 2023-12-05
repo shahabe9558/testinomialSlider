623eb09569b2c7233ee50f518d75c0bc61861b43
Shahabenawaz Alam <shahabe9559@gmail.com>
2023-12-05 14:00:14 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p className='bg-red-400'>HIii</p>
+    </div>
+  );
+}
+
+export default App;
