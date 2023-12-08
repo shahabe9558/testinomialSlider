220b3694057c4e67bae318e39f3eec74e7b29bbb
Shahabenawaz Alam <shahabe9559@gmail.com>
2023-12-08 17:06:17 +0530

    logic added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 
+import { useState } from 'react';
 import './App.css';
 import Testinomial from './components/Testinomial';
 import reviews from './data'
+import { FaChevronRight } from "react-icons/fa";
+import { FaChevronLeft } from "react-icons/fa";
+
 function App() {
+ 
+  let [currentIndex, setCurrentIndex] = useState(0);
+
+  function leftHandler()
+  {
+    if(currentIndex < 0)
+    {
+      setCurrentIndex(0);
+    }
+    else
+    {
+      setCurrentIndex(currentIndex - 1);
+    }
+  
+  }
+  function rightHandler()
+  {
+    if(currentIndex > reviews.length -1)
+    {
+      setCurrentIndex(0);
+    }else
+    {
+      setCurrentIndex(currentIndex + 1);
+    }
+  }
   return (
     <div className="App">
       <h1>Our Testinomial</h1>
       <div></div>
-      <Testinomial reviews= {reviews} />
+      <Testinomial reviews= {reviews[currentIndex]} />
+      {/* button section  */}
+      <div>
+        <button onClick={leftHandler}>
+          <FaChevronLeft/>
+        </button>
+        <button onClick={rightHandler}>
+          <FaChevronRight/>
+        </button>
+        <button>
+          Suprise Me
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/Testinomial.jsx
+++ src/components/Testinomial.jsx
@@ -1,7 +1,19 @@
-const Testinomial = () => {
+import { FaQuoteLeft } from "react-icons/fa";
+import { FaQuoteRight } from "react-icons/fa";
+
+
+const Testinomial = ({reviews}) => {
     return(
        <div>
-        This is testinomial
+            <div></div>
+            <img src={reviews.image} />
+            <h1> {reviews.name} </h1>
+            <h2> {reviews.job} </h2>
+            <div>
+                <FaQuoteLeft/>
+                <p> {reviews.text} </p>
+                <FaQuoteRight/>
+            </div>   
        </div>
     )
 }
